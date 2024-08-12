72bfca905c0930804015d043188b5b9756f98363
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-12 22:15:38 +0300

    Create modifydata.js

---
+++ modifydata.js
@@ -0,0 +1,12 @@
+const data ={
+user: {
+isLoggedIn: true,
+username: 'Deen',
+avatar: 'puppy',
+favourites: ['Python','Javascript','java']
+  },
+tpoResults:{
+amount: 3,
+scores: [120,100,99]
+ }
+}
