c8dea5e5e9112b345004a59b7443bf437493862c
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-14 16:30:09 +0300

    Create forin.js

---
+++ forin.js
@@ -0,0 +1,11 @@
+const myObj ={
+  1: "Deen Dev",
+  2: "Ramah Dev",
+  3: "Zaim Dev",
+  4: "Taliah Dev",
+  5: "Rotich Dev",
+};
+
+for(const key in myObj){
+  console.log(myObj[key].toUpperCase());
+}
