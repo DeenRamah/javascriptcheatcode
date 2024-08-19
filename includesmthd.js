1f49e68aad8c4641c51680e66356d8a8fb2492df
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-19 17:46:13 +0300

    Create includesmthd.js

---
+++ includesmthd.js
@@ -0,0 +1,10 @@
+/to check either a string or array contains a specific value or not. the method returns a boolean
+
+const string ='javascript'
+
+string.includes('J') //output true
+
+const hearts = ['','','']
+
+console.log(hearts.includes(''));
+console.log(hearts.includes(''));
