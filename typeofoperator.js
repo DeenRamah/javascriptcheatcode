4dbcbaba952df5a7dab86c03d23bacead1623675
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-19 17:43:44 +0300

    Create typeofoperator.js

---
+++ typeofoperator.js
@@ -0,0 +1,10 @@
+//type of operator allows you to check type of the value
+
+console.log(typeof 42);
+//output: number
+
+console.log(typeof 'markdown');
+//output: string
+
+console.log(typeof true);
+//output : boolean
